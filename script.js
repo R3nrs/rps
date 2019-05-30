c830e28790a141fdd6df8d387f84ebd2d52e1be4
Renars Robergs <rencha1102@gmail.com>
2019-05-30 10:00:06 +0300

    6th

--- script.js
+++ script.js
@@ -38,3 +38,82 @@ $('.rock').click(function() {
   /*console.log("Player Rolled " + playerRoll);
   console.log("computer Rolled " + computerRoll);*/
 });//end of rock click
+
+//paper click
+$('.paper').click(function() {
+    playerRoll = 2;
+    var computerRoll = Math.floor(Math.random() * 3 + 1);
+    
+    //draw
+    if (computerRoll === playerRoll) {
+      gameMessage.fadeOut(300, function() {
+        gameMessage.html('').append('<p class="game-draw">Draw!</p>' + '<p class="computer-message">The Computer chose paper too!</p>').fadeIn(300);
+        
+        draws++;
+        $('#draws').html('').append(draws);
+      });
+    //paper vs AI rock
+    } else if (playerRoll === 2 && computerRoll === 1) {
+      gameMessage.fadeOut(300, function() {
+        gameMessage.html('').append('<p class="game-won">You Won!</p>' + '<p class="computer-message">The Computer chose rock!</p>').fadeIn(300);
+        
+        playerWin++;
+        $('#wins').html('').append(playerWin);
+      });
+    //paper vs AI scissors
+    } else {
+      gameMessage.fadeOut(300, function() {
+        gameMessage.html('').append('<p class="game-lose">You lost</p>' + '<p class="computer-message">The Computer chose scissors</p>').fadeIn(300);
+        
+        aiWin++;
+        $('#losses').html('').append(aiWin);
+      });
+    }
+    
+    /*console.log("Player Rolled " + playerRoll);
+    console.log("computer Rolled " + computerRoll);*/
+  });//end of paper click
+  
+  //scissors click
+  $('.scissors').click(function() {
+    playerRoll = 3;
+    var computerRoll = Math.floor(Math.random() * 3 + 1);
+    //draw
+    if (computerRoll === playerRoll) {
+      gameMessage.fadeOut(300, function() {
+        gameMessage.html('').append('<p class="game-draw">Draw!</p>' + '<p class="computer-message">The Computer chose scissors too!</p>').fadeIn(300);
+        
+        draws++;
+        $('#draws').html('').append(draws);
+      });
+    //scissors vs AI paper
+    } else if (playerRoll === 3 && computerRoll === 2) {
+      gameMessage.fadeOut(300, function() {
+        gameMessage.html('').append('<p class="game-won">You Won!</p>' + '<p class="computer-message">The Computer chose paper</p>').fadeIn(300);
+        
+        playerWin++;
+        $('#wins').html('').append(playerWin);
+      });
+    //scissors vs AI rock
+    } else {
+      gameMessage.fadeOut(300, function() {
+        gameMessage.html('').append('<p class="game-lose">You lost</p>' + '<p class="computer-message">The Computer chose rock</p>').fadeIn(300);
+        
+        aiWin++;
+        $('#losses').html('').append(aiWin);
+      });
+    }//end of scissors click
+    
+    /*console.log("Player Rolled " + playerRoll);
+    console.log("computer Rolled " + computerRoll);*/
+  });
+  
+  //press "Reset scores" to reset all scores
+  $('#reset-scores').click(function() {
+    playerWin = 0;
+    $('#wins').html(playerWin);
+    draws = 0;
+    $('#draws').html(draws);
+    aiWin = 0;
+    $('#losses').html(aiWin);
+  });
